c0fa1793a11fba0558f8f36ae12dcfda78d98a01
guguan123 <guguanming@outlook.com>
2024-06-06 00:05:31 +0800

    完善客户端IP获取方式

--- announce/index.php
+++ announce/index.php
@@ -13,15 +13,43 @@ if ($conn->connect_error) {
 }
 
 // 从GET请求中获取必要的参数
-$info_hash = $_GET['info_hash'] ?? null;
-$peer_id = $_GET['peer_id'] ?? null;
-$port = $_GET['port'] ?? null;
-$ipv4 = $_GET['ipv4'] ?? null;
-$ipv6 = $_GET['ipv6'] ?? null;
-$downloaded = $_GET['downloaded'];
-$left = $_GET['left'];
-$uploaded = $_GET['uploaded'];
+$info_hash = $_GET['info_hash'] ?? null;    // 需要下载的资源的 torrent 文件的 info 部分的 SHA1 值，或者磁力链接中的 xt 值。
+$peer_id = $_GET['peer_id'] ?? null;        // 标示这个客户端的字符串。
+$port = $_GET['port'] ?? null;              // peer 监听的端口。
+$ipv4 = $_GET['ipv4'] ?? null;              // (可选) peer 的 IPv4
+$ipv6 = $_GET['ipv6'] ?? null;              // (可选) peer 的 IPv6
+$downloaded = $_GET['downloaded'] ?? null;  // 该资源至今为止的下载字节数。
+$left = $_GET['left'] ?? null;              // 该资源剩余未完成下载的字节数。
+$uploaded = $_GET['uploaded'] ?? null;      // 该资源至今为止的上传字节数。
 $supportcrypto = isset($_GET['supportcrypto']) ? 1 : 0;
+$numwant = isset($_GET['numwant']) ? intval($_GET['numwant']) : 50; // (可选) 客户端希望从 tracker 接收的 peers 数量。
+$event = $_GET['event'] ?? null;            // (可选)当前资源下载状态
+$compact = $_GET['compact'] ?? null;        // (可选)客户端希望 tracker 返回压缩 peer 列表。
+
+if (empty($ipv4) && empty($ipv6)) {
+    // 如果peer没通过"ipv4"和"ipv6"字段报告IP信息
+    $ip = $_GET['ip'] ?? null;
+    if ($ip) {
+        // 检测"ip"字段的内容是IPv4还是IPv6
+        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            $ipv4 = $ip;
+        } elseif (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            $ipv6 = $ip;
+        } else {
+            // 报告IP格式错误
+            die(bencode(['failure reason' => 'Invalid IP']));
+        }
+    } else {
+        // 如果客户端没报告IP地址信息，获取IPv4地址
+        $ipv4 = $_SERVER['REMOTE_ADDR'];
+    }
+
+}
+
+// 只允许最多获取1000个peers
+if ($numwant > 1000) {
+    $numwant = 1000;
+}
 
 // 检查必要参数并输出具体的错误信息
 if (!$info_hash) {
@@ -30,11 +58,6 @@ if (!$info_hash) {
     die(bencode(['failure reason' => 'missing peer_id']));
 } elseif (!$port) {
     die(bencode(['failure reason' => 'missing port']));
-}
-
-if (empty($ipv4) && empty($ipv6)) {
-    // 如果客户端没报告IP地址信息，获取IPv4地址
-    $ipv4 = $_SERVER['REMOTE_ADDR'];
 }
 
 // 准备SQL语句用于更新或插入peer信息
@@ -53,8 +76,8 @@ try {
     $delSql = "DELETE FROM peers WHERE updated_at < DATE_SUB(NOW(), INTERVAL 1 HOUR)";
     $conn->query($delSql);
 
-    // 获取当前info_hash下的所有peers（最多50个），排除当前peer_id
-    $getSql = "SELECT ipv4, ipv6, port FROM peers WHERE info_hash = ? AND peer_id != ? LIMIT 50";
+    // 获取当前info_hash下的所有peers，排除当前peer_id
+    $getSql = "SELECT ipv4, ipv6, port FROM peers WHERE info_hash = ? AND peer_id != ? LIMIT $numwant";
     $stmt = $conn->prepare($getSql);
     $stmt->bind_param("ss", $info_hash, $peer_id);
     $stmt->execute();
